7fe3df29b655a8a4a0b7f41efd213dcfb5543182
Greg Ogun <gregogun97@gmail.com>
2021-04-14 21:59:37 +0100

    chore: :heavy_plus_sign: add main dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/react'
+  ],
+  plugins: ['react', 'import', 'jsx-a11y'],
+  rules: {
+    'no-console': 1,
+    'no-unused-vars': 1,
+    'react/prop-types': [
+      'enabled',
+      { ignore: 'ignore', customValidators: 'customValidator' }
+    ]
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! </a>
         </h1>
 
         <p className={styles.description}>
@@ -61,5 +61,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
