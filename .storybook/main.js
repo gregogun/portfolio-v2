985c2de74ac6bcbeed70b6be6edaed5f95e97f1e
Greg Ogun <gregogun97@gmail.com>
2021-04-17 22:51:43 +0100

    chore: :wrench: update Storybook config for use with Chakra UI

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,24 @@
+const path = require('path');
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react')
+        }
+      }
+    };
+  }
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,20 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from '../styles/theme';
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider theme={customTheme}>
+      <Story />
+    </ChakraProvider>
+  )
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
