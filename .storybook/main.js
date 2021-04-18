329a1ec84f30e7c9a5ab7fc5242327dc7d961f53
Greg Ogun <gregogun97@gmail.com>
2021-04-18 19:05:55 +0100

    chore: :wrench: update config with a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,11 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y'
+  ],
   webpackFinal: async (config) => {
     return {
       ...config,
