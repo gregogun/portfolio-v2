8ac4ca7ee9ace679b57a4bd13d535a9c305bc632
Greg Ogun <gregogun97@gmail.com>
2021-04-18 13:35:23 +0100

    chore: :lipstick: update Chakra UI theme
    added button base styles according to design, updated primary palette and added color mode settings to config.

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -10,6 +10,93 @@ const theme = extendTheme({
       }
     })
   },
+  components: {
+    Button: {
+      // Styles for the base style
+      baseStyle: {
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        borderRadius: 'sm'
+      },
+      // Styles for the size variations
+      sizes: {
+        sm: {
+          fontSize: 'xs',
+          px: '0.75rem',
+          py: '0.5rem'
+        },
+        md: {
+          fontSize: 'md',
+          px: '1rem',
+          py: '0.5rem'
+        },
+        lg: {
+          fontSize: 'lg',
+          px: '1.5rem',
+          py: '0.75rem'
+        }
+      },
+      // Styles for the visual style variations
+      variants: {
+        primary: {
+          border: '1px solid',
+          borderColor: 'default.dark',
+          color: 'default.dark',
+          _hover: {
+            bg: 'default.dark',
+            color: 'default.light',
+            border: 'none'
+          },
+          _active: {
+            bg: 'default.dark',
+            color: 'default.light',
+            border: 'none'
+          },
+          _disabled: {
+            borderColor: 'neutral.200',
+            color: 'neutral.200'
+          }
+        },
+        secondary: {
+          bg: 'default.dark',
+          color: 'default.light',
+          _hover: {
+            bg: 'neutral.600'
+          },
+          _active: {
+            bg: 'default.dark'
+          },
+          _disabled: {
+            bg: 'neutral.100',
+            color: 'neutral.200'
+          }
+        },
+        primaryThemed: (props) => ({
+          border: '1px solid',
+          borderColor:
+            props.colorMode === 'light' ? 'primary.500' : 'secondary.300',
+          color: props.colorMode === 'light' ? 'primary.500' : 'secondary.300',
+          _hover: {
+            bg: props.colorMode === 'light' ? 'primary.600' : 'secondary.400',
+            color:
+              props.colorMode === 'light' ? 'default.light' : 'default.dark'
+          }
+        }),
+        secondaryThemed: (props) => ({
+          bg: props.colorMode === 'light' ? 'primary.500' : 'secondary.300',
+          color: props.colorMode === 'light' ? 'default.light' : 'default.dark',
+          _hover: {
+            bg: props.colorMode === 'light' ? 'primary.600' : 'secondary.400'
+          }
+        })
+      },
+      // The default `size` or `variant` values
+      defaultProps: {
+        size: 'md',
+        variant: 'primary'
+      }
+    }
+  },
   colors: {
     default: {
       light: '#fcfcfc',
@@ -18,10 +105,10 @@ const theme = extendTheme({
     primary: {
       100: '#D1C1F4',
       200: '#B199E7',
-      300: '#9171DC',
-      400: '#7B4ECB',
+      300: '#9C7DE4',
+      400: '#8F67D7',
       500: '#805AD5',
-      600: '#45248F',
+      600: '#744EC8',
       700: '#311968'
     },
     secondary: {
@@ -33,7 +120,7 @@ const theme = extendTheme({
       600: '#319795',
       700: '#2C7A7B'
     },
-    neutrals: {
+    neutral: {
       100: '#F4F4F4',
       200: '#D9D9D9',
       300: '#A6A6A6',
@@ -46,6 +133,10 @@ const theme = extendTheme({
   fonts: {
     heading: 'Sora',
     body: 'Sora'
+  },
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false
   }
 });
 
