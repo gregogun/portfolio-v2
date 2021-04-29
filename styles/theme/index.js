2ee9406bef8503569b04b1090ddf43912fa06681
Greg Ogun <gregogun97@gmail.com>
2021-04-29 23:12:31 +0100

    style: :lipstick: add custom breakpoints to theme

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -1,10 +1,21 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 import { styles } from './styles';
 import { colors } from './colors';
 import { Heading, Text, fonts, textStyles } from './typography';
 import { Button } from './components/button';
 
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+  '2xl': '90em',
+  '3xl': '120em'
+});
+
 const overrides = {
+  breakpoints,
   styles,
   colors,
   fonts,
