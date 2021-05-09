c14f1b061f7b34ba8d47a4482b2412764bde3072
Greg Ogun <65421744+gregogun@users.noreply.github.com>
2021-05-09 21:22:05 +0100

    style/update theme
    * style: update default border radius
    
    * style: add accent colour styles
    
    * style: add success colours to color switch hook
    
    * style: add fallback fonts to theme
    
    * chore: remove unused code
    
    * feat: add story for input component

---
+++ components/stories/Input.stories.js
@@ -0,0 +1,33 @@
+import { Input } from '@chakra-ui/react';
+
+export default {
+  title: 'Style Guide/Components/Input',
+  component: Input
+};
+
+const Template = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: 'me@gregogun.com',
+  w: '17.5rem',
+  borderRadius: 'sm'
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Default.args,
+  isDisabled: true
+};
+
+export const Invalid = Template.bind({});
+Invalid.args = {
+  ...Default.args,
+  isInvalid: true
+};
+
+export const Required = Template.bind({});
+Required.args = {
+  ...Default.args,
+  isRequired: true
+};
