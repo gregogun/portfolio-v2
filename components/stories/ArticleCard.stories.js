f819616974cc0b18a272fc5206e81a1d09242b1a
gregogun <gregogun97@gmail.com>
2021-05-09 17:20:27 +0100

    feat: add story for article card

---
+++ components/stories/ArticleCard.stories.js
@@ -0,0 +1,10 @@
+import { ArticleCard } from '../../pages/index';
+
+export default {
+  title: 'Style Guide/Components/Card/ArticleCard',
+  component: ArticleCard
+};
+
+const Template = (args) => <ArticleCard {...args} />;
+
+export const Default = Template.bind({});
