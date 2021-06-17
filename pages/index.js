0473701fa64601c240bfd3f1b56fc18f5d65ad77
Greg Ogun <gregogun97@gmail.com>
2021-06-17 17:40:04 +0100

    fix: merge conflicts

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ import projects from '@/data/projects';
 import { ProjectCard } from '@/components/projectCard';
 import { ContentWrapper } from '@/layouts/contentWrapper';
 import { css } from '@emotion/react';
-import { useColorSwitcher } from '@/utils/hooks/useColorModeSwitcher';
 import { BsArrowDown } from 'react-icons/bs';
 import { useEffect, useState } from 'react';
 
@@ -115,7 +114,6 @@ const ScrollArrow = ({ scrollPos }) => {
         fill={colorDark}
         css={css`
           animation: arrowAnimation 1.5s infinite ease-in-out;
-
           @keyframes arrowAnimation {
             0% {
               opacity: 0;
@@ -133,143 +131,6 @@ const ScrollArrow = ({ scrollPos }) => {
         `}
       />
     </VStack>
-  );
-};
-
-const FeaturedProjects = () => {
-  return (
-    <VStack spacing="4rem" w="100%" m="auto">
-      <FeatureHeading>Featured Projects</FeatureHeading>
-      <Flex
-        w="100%"
-        mb="2.5rem"
-        direction={{ base: 'column', xl: 'row' }}
-        justify="space-evenly"
-      >
-        <Projects />
-      </Flex>
-      <NextLink href="/projects" passHref>
-        <Button
-          as="a"
-          textTransform="capitalize"
-          display="block"
-          textAlign="center"
-          fontSize={{ base: 'lg', lg: 'xl' }}
-          variant="secondaryThemed"
-        >
-          See all projects
-        </Button>
-      </NextLink>
-    </VStack>
-  );
-};
-
-const Projects = () => {
-  return (
-    <List
-      mx="auto"
-      justifyContent="space-between"
-      display={{ base: 'block', '2xl': 'flex' }}
-    >
-      {projects
-        .filter((project) => project.feature)
-        .map((project) => (
-          <ProjectCard
-            data-testid="project-card"
-            logo={project.logo}
-            title={project.title}
-            description={project.description}
-            tools={project.tools}
-            live={project.live}
-            proto={project.proto}
-            repo={project.repo}
-            key={project.id}
-          />
-        ))}
-    </List>
-  );
-};
-
-const FeaturedArticles = () => {
-  return (
-    <VStack spacing="4rem" w="100%" mx="auto">
-      <FeatureHeading>Featured Articles</FeatureHeading>
-      <ArticleCard />
-      <ArticleCard />
-      <NextLink href="/blog" passHref>
-        <Link
-          display="block"
-          textAlign="center"
-          fontSize={{ base: 'lg', lg: '2xl' }}
-        >
-          Read all articles
-        </Link>
-      </NextLink>
-    </VStack>
-  );
-};
-
-export const ArticleCard = () => {
-  const { colorGrey } = useColorModeSwitcher();
-  return (
-    <Box
-      mx="auto"
-      p="2rem"
-      border="2px solid"
-      borderColor={colorGrey}
-      w={{ base: '21em', lg: '57.5rem' }}
-    >
-      <Heading textTransform="capitalize" as="h4" variant="h4" mb="0.5rem">
-        A heading for my amazing blog post
-      </Heading>
-      <Text variant="body" mb="2rem">
-        This is some arbitrary subtitle for my amazing blog post that you should
-        most definitely read!
-      </Text>
-      <Flex>
-        <Text>Read more</Text>
-        <Text>
-          <span>→</span>
-        </Text>
-      </Flex>
-    </Box>
-  );
-};
-
-const Hero = () => {
-  const [isXtraLarge] = useMediaQuery('(min-width: 1280px)');
-  return (
-    <Flex justify="space-between" w={{ base: '90%', sm: '100%' }}>
-      <Box
-        // m={!isXtraLarge && 'auto'}
-        w={{ base: '100%', xl: '50%' }}
-      >
-        <Heading mb="0.5rem" as="h1" variant="h1">
-          Hi, I’m Greg Ogun.
-        </Heading>
-        <Text mb={{ base: '4rem', lg: '6rem' }} as="h2" variant="subtitle">
-          Welcome to my corner of the internet, where you can find my works,
-          thoughts, favourite beer and other random things.
-        </Text>
-        <Heading as="h3" variant="h3" mb="0.5rem">
-          Ok, but who are you?
-        </Heading>
-        <Text fontSize="xl" mb="2rem">
-          I’m a curiosity-driven, design-focused developer, building rich,
-          user-centric, inclusive experiences on the web.
-        </Text>
-        <Text fontSize="xl" mb={{ base: '1rem', lg: '2rem' }}>
-          Feel free to have a look around, and learn more about myself and what
-          I like to work on.{' '}
-        </Text>
-        <NextLink href="/about#contact" passHref>
-          <Button as="a" variant="primaryThemed" size="lg">
-            Get in touch
-          </Button>
-        </NextLink>
-      </Box>
-      {isXtraLarge && <HeroVisual />}
-    </Flex>
   );
 };
 
