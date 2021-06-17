49bfd3c9419e35ce8582d44a14a9485137d68e9d
Greg Ogun <65421744+gregogun@users.noreply.github.com>
2021-06-17 07:01:11 +0100

    feature/add animations (#41)
    * feat: add scroll arrow and logic to hide and show it
    
    * feat: convert hero visual elements to motion components

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import {
   useMediaQuery,
   Button,
   Link,
-  List
+  List,
+  Icon
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import Container from '@/layouts/container';
@@ -17,6 +18,10 @@ import Subscribe from '@/components/subscribe';
 import projects from '@/data/projects';
 import { ProjectCard } from '@/components/projectCard';
 import { ContentWrapper } from '@/layouts/contentWrapper';
+import { css } from '@emotion/react';
+import { useColorSwitcher } from '@/utils/hooks/useColorModeSwitcher';
+import { BsArrowDown } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
 
 export default function Homepage() {
   return (
@@ -32,38 +37,102 @@ export default function Homepage() {
 
 const Hero = () => {
   const [isXtraLarge] = useMediaQuery('(min-width: 1280px)');
+  const [scrollPos, setScrollPos] = useState();
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScroll);
+
+    return () => {
+      window.removeEventListener('scroll', listenScroll);
+    };
+  });
+
+  const listenScroll = () => {
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrolledFloat = windowScroll / height;
+
+    const scrolled = scrolledFloat > 0.05;
+
+    setScrollPos(scrolled);
+  };
+
   return (
-    <Flex justify="space-between" w={{ base: '90%', sm: '100%' }}>
-      <Box
-        // m={!isXtraLarge && 'auto'}
-        w={{ base: '100%', xl: '50%' }}
-      >
-        <Heading mb="0.5rem" as="h1" variant="h1">
-          Hi, I’m Greg Ogun.
-        </Heading>
-        <Text mb={{ base: '4rem', lg: '6rem' }} as="h2" variant="subtitle">
-          Welcome to my corner of the internet, where you can find my works,
-          thoughts, favourite beer and other random things.
-        </Text>
-        <Heading as="h3" variant="h3" mb="0.5rem">
-          Ok, but who are you?
-        </Heading>
-        <Text fontSize="xl" mb="2rem">
-          I’m a curiosity-driven, design-focused developer, building rich,
-          user-centric, inclusive experiences on the web.
-        </Text>
-        <Text fontSize="xl" mb={{ base: '1rem', lg: '2rem' }}>
-          Feel free to have a look around, and learn more about myself and what
-          I like to work on.{' '}
-        </Text>
-        <NextLink href="/about#contact" passHref>
-          <Button as="a" variant="primaryThemed" size="lg">
-            Get in touch
-          </Button>
-        </NextLink>
-      </Box>
-      {isXtraLarge && <HeroVisual />}
-    </Flex>
+    <Box w={{ base: '90%', '2xl': '100%' }}>
+      <Flex mb="4rem" justify="space-between">
+        <Box
+          // m={!isXtraLarge && 'auto'}
+          w={{ base: '100%', xl: '50%' }}
+        >
+          <Heading mb="0.5rem" as="h1" variant="h1">
+            Hi, I’m Greg Ogun.
+          </Heading>
+          <Text mb={{ base: '4rem', lg: '6rem' }} as="h2" variant="subtitle">
+            Welcome to my corner of the internet, where you can find my works,
+            thoughts, favourite beer and other random things.
+          </Text>
+          <Heading as="h3" variant="h3" mb="0.5rem">
+            Ok, but who are you?
+          </Heading>
+          <Text fontSize="xl" mb="2rem">
+            I’m a curiosity-driven, design-focused developer, building rich,
+            user-centric, inclusive experiences on the web.
+          </Text>
+          <Text fontSize="xl" mb={{ base: '1rem', lg: '2rem' }}>
+            Feel free to have a look around, and learn more about myself and
+            what I like to work on.{' '}
+          </Text>
+          <NextLink href="/about#contact" passHref>
+            <Button as="a" variant="primaryThemed" size="lg">
+              Get in touch
+            </Button>
+          </NextLink>
+        </Box>
+        {isXtraLarge && <HeroVisual />}
+      </Flex>
+      {isXtraLarge && <ScrollArrow scrollPos={scrollPos} />}
+    </Box>
+  );
+};
+
+const ScrollArrow = ({ scrollPos }) => {
+  const { colorDark } = useColorModeSwitcher();
+  return (
+    <VStack
+      opacity={scrollPos ? 0 : 1}
+      transition="visibility 0s linear 300ms, opacity 600ms"
+      visibility={scrollPos ? 'hidden' : 'visible'}
+    >
+      <span>Scroll</span>
+      <Icon
+        as={BsArrowDown}
+        boxSize="1.5em"
+        fill={colorDark}
+        css={css`
+          animation: arrowAnimation 1.5s infinite ease-in-out;
+
+          @keyframes arrowAnimation {
+            0% {
+              opacity: 0;
+            }
+            30% {
+              opacity: 1;
+            }
+            60% {
+              opacity: 1;
+            }
+            to {
+              opacity: 0;
+            }
+          }
+        `}
+      />
+    </VStack>
   );
 };
 
